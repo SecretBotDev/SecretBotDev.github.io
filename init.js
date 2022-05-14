4c893c6db4526a76d976cbdddea0ac27d966518a
Moritz <90388353+moritz-t-w@users.noreply.github.com>
2022-05-14 16:46:09 +0200

    unified comments

--- init.js
+++ init.js
@@ -1,19 +1,19 @@
 function includeHTML() {
 	let z, i, elmnt, file, xhttp;
-	/*loop through a collection of all HTML elements:*/
+	//loop through a collection of all HTML elements:
 	z = document.getElementsByTagName("*");
 	for (i = 0; i < z.length; i++) {
 		elmnt = z[i];
-		/*search for elements with a certain atrribute:*/
+		//search for elements with a certain atrribute:
 		file = elmnt.getAttribute("w3-include-html");
 		if (file) {
-			/*make an HTTP request using the attribute value as the file name:*/
+			//make an HTTP request using the attribute value as the file name:
 			xhttp = new XMLHttpRequest();
 			xhttp.onreadystatechange = function () {
 				if (this.readyState == 4) {
 					if (this.status == 200) { elmnt.innerHTML = this.responseText; }
 					if (this.status == 404) { elmnt.innerHTML = "Error while loading."; }
-					/*remove the attribute, and call this function once more:*/
+					//remove the attribute, and call this function once more:
 					elmnt.removeAttribute("w3-include-html");
 					includeHTML();
 				}
